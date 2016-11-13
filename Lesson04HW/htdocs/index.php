0306fd3aeb36fd7296205213a664ed5288f887e0
malmax <malmax.spb@gmail.com>
2016-11-13 23:26:45 +0300

    add menu to temporary

--- Lesson04HW/htdocs/index.php
+++ Lesson04HW/htdocs/index.php
@@ -2,24 +2,38 @@
 require_once('../config.php');
 
 $vars = array(
-    'index' =>
+  'index' =>
     array(
-        'TITLE' => 'Главная страница',
-        'HEADER' => 'Новость',
-        'CONTENT' => 'Идет занятие'
+      'TITLE' => 'Главная страница',
+      'HEADER' => 'Новость',
+      'CONTENT' => 'Идет занятие',
+      'menu' => get_menu(),
     ),
-    'about' =>
+  'about' =>
     array(
-        'TITLE1' => 'О сайте',
-        'HEADER1' => 'Создатели',
-        'CONTENT1' => 'Команда программистов',
-        'DIRECTORNAME1' => 'Имя директора'
+      'TITLE1' => 'О сайте',
+      'HEADER1' => 'Создатели',
+      'CONTENT1' => 'Команда программистов',
+      'DIRECTORNAME1' => 'Имя директора',
+      'menu' => get_menu(),
+    ),
+  'news' =>
+    array(
+      'TITLE' => 'Новости',
+      'HEADER' => 'Главные новости магазина',
+      'CONTENT' => 'Поздравляем, наш магазин игр открылся. Добро пожаловать',
+      'menu' => get_menu(),
     ),
 );
 
 //echo render('index');
+if (isset($_GET['q']) && in_array($_GET['q'], array_keys($vars))) {
+  $page_r = $_GET['q'];
+}
+else {
+  $page_r = 'index';
+}
 
-$page_r = 'index';
 
 echo render($page_r, $vars[$page_r]);
 
--- Lesson04HW/lib/functions.php
+++ Lesson04HW/lib/functions.php
@@ -5,36 +5,64 @@ define('ERROR_TEMPLATE_EMPTY', 2);
 
 // Обрабатывает указанный шаблон, подставляя нужные переменные
 
-function render($file, $variables = array())
-{
-    $file = TPL_DIR  . '/' . $file . '.html'; // делаем защиту от ненужных типов файлов
-    if (!is_file($file)) {
-      	echo 'Template file "' . $file . '" not found';
-      	exit(ERROR_NOT_FOUND);
+function render($file, $variables = array()) {
+  $file = TPL_DIR . '/' . $file . '.html'; // делаем защиту от ненужных типов файлов
+  if (!is_file($file)) {
+    echo 'Template file "' . $file . '" not found';
+    exit(ERROR_NOT_FOUND);
+  }
+
+  if (filesize($file) === 0) {
+    echo 'Template file "' . $file . '" is empty';
+    exit(ERROR_TEMPLATE_EMPTY);
+  }
+
+  // если переменных для подстановки не указано, просто
+  // возвращаем шаблон как есть
+  if (empty($variables)) {
+    $templateContent = file_get_contents($file);
+  }
+  else {
+    $templateContent = file_get_contents($file);
+    foreach ($variables as $key => $value) {
+
+      if ($value != NULL) {
+        switch ($key) {
+          case 'menu':
+            if (is_array($value)) {
+              $out = "<ul>";
+              foreach ($value as $k => $v) {
+                if ($key) {
+                  $out .= "<li><a href='./index.php?q=$v'>$k</a></li>";
+                }
+              }
+              $out .= "</ul>";
+
+              $value = $out;
+            }
+            break;
+        }
+
+
+        // собираем ключи
+        $key = '((' . strtoupper($key) . '))';
+
+        // заменяем ключи на значения в теле шаблона
+        $templateContent = str_replace($key, $value, $templateContent);
+      }
     }
+  }
 
-    if (filesize($file) === 0) {
-      	echo 'Template file "' . $file . '" is empty';
-      	exit(ERROR_TEMPLATE_EMPTY);
-    }
-
-    // если переменных для подстановки не указано, просто
-    // возвращаем шаблон как есть
-    if (empty($variables)) {
-	    $templateContent = file_get_contents($file);
-    } else {
-      	$templateContent = file_get_contents($file);
-      	foreach ($variables as $key => $value) {
-              if ($value != null) {
-    	        // собираем ключи
-              $key = '((' . strtoupper($key) . '))';
-
-          		// заменяем ключи на значения в теле шаблона
-          		$templateContent = str_replace($key, $value, $templateContent);
-	            }
-         }
-    }
-
-    return $templateContent;
+  return $templateContent;
 }
+
+function get_menu() {
+  return Array(
+    'Главная' => 'index',
+    'О магазине' => 'about',
+    'Новости' => 'news',
+    'Каталог' => 'catalog'
+  );
+}
+
 ?>
